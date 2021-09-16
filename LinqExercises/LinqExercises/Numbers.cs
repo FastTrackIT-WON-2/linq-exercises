1abec8234616ff9850f65a1318219b2b2dac50b9
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-09-16 19:20:41 +0300

    First sample

---
+++ LinqExercises/LinqExercises/Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExercises
+{
+    public static class Numbers
+    {
+        public static IEnumerable<int> AllNumbers()
+        {
+            int currentNumber = 0;
+            while (true)
+            {
+                yield return currentNumber;
+                if (currentNumber < int.MaxValue)
+                {
+                    currentNumber++;
+                }
+                else
+                {
+                    currentNumber = 0;
+                }
+            }
+        }
+    }
+}
---
+++ LinqExercises/LinqExercises/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static void Sample1_SelectEvenNumbers()
+        {
+            /* SQL-like syntax: 
+            var query = from number in Numbers.AllNumbers()
+                        where number % 2 == 0
+                        select number;
+            */
+
+            var query = Numbers.AllNumbers()
+                               .Where(number => number % 2 == 0);
+
+            Console.Write("How many even numbers to display?=");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            foreach (int evenNumber in query)
+            {
+                Console.Write($"{evenNumber}, ");
+                i++;
+                if (i >= n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
